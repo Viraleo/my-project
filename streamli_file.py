42054e4ec97f8065bc288f3556b83a19002cb01f
Viraleo <111207310+Viraleo@users.noreply.github.com>
2023-08-15 09:38:01 -0500

    5

--- streamli_file.py
+++ streamli_file.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 st.title("My first streamlit app")
@@ -9,7 +10,7 @@ st.header("This is about bike")
 st.sidebar.title ("Here's going to be some information")
 
 def load_dataset(data_link):
-    dataset = pd.read_csv(data_link)
+    dataset = pd.read_csv(data_link, index_col = "datetime", parse_dates=True))
     return dataset
 
 
@@ -17,4 +18,9 @@ bike_link = 'https://gitlab.com/codedeploycloud/data_science/-/raw/main/datasets
 bike_data = load_dataset(bike_link)
 
 st.dataframe(bike_data)
-st.line_chart(bike_data)
+bike_data["hour"]=bike_data.index.hour
+bike.rename(columns={'count': "total_count"}, inplace= True)
+
+fig, ax = plt.subplots()
+ax.plot(bike_data['hour'])
+st.pyplot(fig)
