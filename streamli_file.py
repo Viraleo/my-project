9dc425e908cbd4c761b752bfa2775a60f8325805
Viraleo <111207310+Viraleo@users.noreply.github.com>
2023-08-15 09:16:02 -0500

    4

--- streamli_file.py
+++ streamli_file.py
@@ -13,7 +13,8 @@ def load_dataset(data_link):
     return dataset
 
 
-titanic_link = 'https://gitlab.com/codedeploycloud/data_science/-/raw/main/datasets/insurance_1.csv'
-titanic_data = load_dataset(titanic_link)
+bike_link = 'https://gitlab.com/codedeploycloud/data_science/-/raw/main/datasets/insurance_1.csv'
+bike_data = load_dataset(bike_link)
 
-st.dataframe(titanic_data)
+st.dataframe(bike_data)
+st.line_chart(bike_data)
