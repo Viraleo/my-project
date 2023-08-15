88dd7b42cb1452ef78f5f3899ae7e04e5aada0aa
Viraleo <111207310+Viraleo@users.noreply.github.com>
2023-08-15 09:12:47 -0500

    1

--- streamli_file.py
+++ streamli_file.py
@@ -3,17 +3,17 @@ import pandas as pd
 import numpy as np
 
 
-st.write("""
-My first app
-Hello *world!*
-""")
+st.title("may first streamlit app")
+st.header (This is about bike")
+
+st.sidebar.title (Here's going to be some information")
 
 def load_dataset(data_link):
     dataset = pd.read_csv(data_link)
     return dataset
 
 
-titanic_link = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv'
+titanic_link = 'https://gitlab.com/codedeploycloud/data_science/-/raw/main/datasets/insurance_1.csv'
 titanic_data = load_dataset(titanic_link)
 
 st.dataframe(titanic_data)
