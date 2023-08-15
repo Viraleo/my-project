756a910b0b9366adbe554583572a0127417bc396
Viraleo <111207310+Viraleo@users.noreply.github.com>
2023-08-15 11:52:41 -0500

    9

--- streamli_file.py
+++ streamli_file.py
@@ -1,9 +1,8 @@
-pip install --upgrade streamlit matplotlib
+
 
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 st.title("My first streamlit app")
